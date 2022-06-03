50377750ee732f862ec866b98a5d3864f0594e1b
Quang <nguyenvietquang25032001@gmail.com>
2022-06-03 18:36:03 -0500

    xcgvb

--- Hotel/Entities/BillEntity.cs
+++ Hotel/Entities/BillEntity.cs
@@ -1,34 +1,13 @@
-﻿namespace Hotel.Entities
+﻿using System;
+
+namespace Hotel.Entities
 {
     class BillEntity
     {
-        public int order_number;
-        public long customer_pay;
+        public int booking_id;
         public long total_price;
-        public string description;
-        public string created_at;
-
-        public BillEntity setOrderNumber(int order_number)
-        {
-            this.order_number = order_number;
-            return this;
-        }
-
-        public BillEntity setCustomerPay(long customer_pay)
-        {
-            this.customer_pay = customer_pay;
-            return this;
-        }
-        public BillEntity setTotalPrice(long total_price)
-        {
-            this.total_price = total_price;
-            return this;
-        }
-        public BillEntity setDescription(string desc)
-        {
-            this.description = desc;
-            return this;
-        }
+        public string desc;
+        public DateTime created_at;
     }
 }
 
